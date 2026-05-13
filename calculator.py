58b544c9edf3a41d57444cdfcb3a0b7fbdf195b2
Scabbe <sebastian.cabbe@gmail.com>
2026-05-13 07:41:56 +0200

    added calculator program

---
+++ calculator.py
@@ -0,0 +1,22 @@
+def add_numbers(a, b):
+    return a + b
+
+def subtract_numbers(a, b):
+    return a - b
+
+
+print("Simple Calculator")
+
+number1 = float(input("Enter first number:"))
+number2 = float(input("Enter second number:"))
+
+operation = input("Choose operation (+ or -): ")
+
+if operation == "+":
+    result = add_numbers(number1, number2)
+elif operation == "-":
+    result = subtract_numbers(number1, number2)
+else:
+    result = "Invalid operation"
+
+print(f"Result: {result}")
