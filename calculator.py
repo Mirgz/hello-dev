d5f2b22c31ee22c96ccce06492a5376b6694cc8c
Scabbe <sebastian.cabbe@gmail.com>
2026-05-13 07:52:27 +0200

    added more functionalities to calculator

--- calculator.py
+++ calculator.py
@@ -7,16 +7,27 @@ def subtract_numbers(a, b):
 
 print("Simple Calculator")
 
-number1 = float(input("Enter first number:"))
-number2 = float(input("Enter second number:"))
+running = True
 
-operation = input("Choose operation (+ or -): ")
+while running:
 
-if operation == "+":
-    result = add_numbers(number1, number2)
-elif operation == "-":
-    result = subtract_numbers(number1, number2)
-else:
-    result = "Invalid operation"
+    number1 = float(input("Enter first number: "))
+    number2 = float(input("Enter second number: "))
 
-print(f"Result: {result}")
+    operation = input("Choose operation (+ or -): ")
+
+    if operation == "+":
+        result = add_numbers(number1, number2)
+    elif operation == "-":
+        result = subtract_numbers(number1, number2)
+    else:
+        result = "Invalid operation"
+
+    print(f"Result: {result}")
+
+    continue_choice = input("Continue? (y/n): ")
+
+    if continue_choice == "n":
+        running = False
+
+print("Calculator closed")
