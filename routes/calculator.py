b3a572e0b9dec4910f515563dcf1e161b1b2729d
Scabbe <sebastian.cabbe@gmail.com>
2026-06-03 21:28:39 +0200

    added service layer and improved api structure

--- routes/calculator.py
+++ routes/calculator.py
@@ -1,5 +1,10 @@
 from fastapi import APIRouter
 from models.numbers import Numbers
+from services.calculator_service import (
+    add_numbers,
+    subtract_numbers,
+    multiply_numbers,
+)
 
 router = APIRouter(
     prefix="/calculator",
@@ -8,12 +13,12 @@ router = APIRouter(
 
 @router.get("/add")
 def add(a: float, b:float):
-    return{"result": a + b}
+    return{"result": add_numbers(a,b)}
 
 @router.get("/subtract")
 def subtract(a: float, b:float):
-    return{"result": a - b}
+    return{"result": subtract_numbers(a,b)}
 
 @router.post("/multiply")
 def multiply(numbers: Numbers):
-    return{"result": numbers.a * numbers.b}
+    return{"result": multiply_numbers(numbers.a, numbers.b)}
---
+++ services/calculator_service.py
@@ -0,0 +1,8 @@
+def add_numbers(a: float, b: float):
+    return a + b
+
+def subtract_numbers(a: float, b: float):
+    return a - b
+
+def multiply_numbers(a: float, b: float):
+    return a * b
